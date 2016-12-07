e176e9e549288e8a3d620b3c009a1cdfb5ba02be
Tyler <tch6zf@virginia.edu>
2016-12-07 18:34:25 -0500

    commented out code for plots we don't need for presentation on 12/8

--- plots.R
+++ plots.R
@@ -1,31 +1,35 @@
 require(dplyr)
 require(tidyr)
+require(ggplot2)
+
 # load in location data
-load("/Users/tomoei/Dropbox/DMV project/data/location.Rdata")
-location <- group_by(location, Year, CrashTypeName)
+#load("/Users/tomoei/Dropbox/DMV project/data/location.Rdata")
+location <- read.csv("location.csv") %>% 
+  group_by(Year, CrashTypeName)
 table(location$CrashTypeName)
-crashType = c("Fatal Crash","Injury Crash","Property Damage Crash")
+crashType = c("Fatal Crash","Injury Crash","Property Damage Crash", NA)
+#sum(is.na(location$CrashTypeName))   121865 unknown crash types (NA)
 
 # plot number of crashes by year (three types in on graph)
 by_year <- summarise(location, n=n())
 by_year <- na.omit(by_year)
-for (i in c(1:3)){
-  par(new = TRUE)
-  plot(by_year$Year[by_year$CrashTypeName == crashType[i]], by_year$n[by_year$CrashTypeName == crashType[i]],
-       ylim = c(600,82000), ylab = "Number of Crashes", xlab = "Year",
-       main = "Number of crash by year",
-       pch = '*', type = "o", col = i)
-}
+#for (i in c(1:3)){
+#  par(new = TRUE)
+#  plot(by_year$Year[by_year$CrashTypeName == crashType[i]], by_year$n[by_year$CrashTypeName == crashType[i]],
+#       ylim = c(600,82000), ylab = "Number of Crashes", xlab = "Year",
+#       main = "Number of crash by year",
+#       pch = '*', type = "o", col = i)
+#}
 
-par(mfrow=c(1,3))
-for (i in c(1:3)){
-  plot(by_year$Year[by_year$CrashTypeName == crashType[i]], by_year$n[by_year$CrashTypeName == crashType[i]],
-       ylab = "Number of Crashes", xlab = "Year",
-       main = "Number of crash by year",
-       pch = '*', type = "o", col = i)
-}
+#par(mfrow=c(1,3))
+#for (i in c(1:3)){
+#  plot(by_year$Year[by_year$CrashTypeName == crashType[i]], by_year$n[by_year$CrashTypeName == crashType[i]],
+#       ylab = "Number of Crashes", xlab = "Year",
+#       main = "Number of crash by year",
+#       pch = '*', type = "o", col = i)
+#}
 
-# plot by crash type and year
+# plot by crash type and year (each one individual but on same row)
 par(mfrow=c(1,3))
 for (i in c(1:3)){
   plot(by_year$Year[by_year$CrashTypeName == crashType[i]], by_year$n[by_year$CrashTypeName == crashType[i]],
@@ -37,46 +41,51 @@ for (i in c(1:3)){
 location$CrashDate <- as.character(location$CrashDate)
 location$month <- lapply(location$CrashDate, function(x) return(strsplit(x,"/")[[1]][1]))
 location$month <- as.numeric(location$month)
-location <- group_by(location, Year, month, CrashTypeName)
-temp <- summarise(location, sum = n())
-temp <- na.omit(temp)
+#location2 <- group_by(location, Year, month, CrashTypeName)
+#temp <- summarise(location2, sum = n())
+#temp <- na.omit(temp)
 
-par(mar = rep(2, 4))
-par(mfrow=c(2,3))
+#par(mar = rep(2, 4))
+#par(mfrow=c(2,3))
 
-for (i in c(2015:2010)){
-  name  = paste(i, " Fatal Crash")
-  plot(temp$month[temp$CrashTypeName == "Fatal Crash" & temp$Year == i], 
-       temp$sum[temp$CrashTypeName == "Fatal Crash" & temp$Year == i],
-       xlab = "Month", ylab = "Number of crashes", main = name,
-       ylim = c(0,90), col = i-2005,
-       pch = '*', type = 'o')
-}
+#for (i in c(2010:2015)){
+#  name  = paste(i, " Fatal Crash")
+#  plot(temp$month[temp$CrashTypeName == "Fatal Crash" & temp$Year == i], 
+#       temp$sum[temp$CrashTypeName == "Fatal Crash" & temp$Year == i],
+#       xlab = "Month", ylab = "Number of crashes", main = name,
+#       ylim = c(0,90), col = i-2005,
+#       pch = '*', type = 'o')
+#}
 
 # plot by month - injury crash
-par(mar = rep(2, 4))
-par(mfrow=c(2,3))
-for (i in c(2015:2010)){
-  name  = paste(i, " Injury Crash")
-  plot(temp$month[temp$CrashTypeName == "Injury Crash" & temp$Year == i], 
-       temp$sum[temp$CrashTypeName == "Injury Crash" & temp$Year == i],
-       xlab = "Month", ylab = "Number of crashes", main = name,col = i-2005,
-       pch = '*', type = 'o')
-}
+#par(mar = rep(2, 4))
+#par(mfrow=c(2,3))
+#for (i in c(2015:2010)){
+#  name  = paste(i, " Injury Crash")
+#  plot(temp$month[temp$CrashTypeName == "Injury Crash" & temp$Year == i], 
+#       temp$sum[temp$CrashTypeName == "Injury Crash" & temp$Year == i],
+#       xlab = "Month", ylab = "Number of crashes", main = name,col = i-2005,
+#       pch = '*', type = 'o')
+#}
 
 # plot by month - property damage crash
-par(mar = rep(2, 4))
-par(mfrow=c(2,3))
-for (i in c(2015:2010)){
-  name  = paste(i, " Property Damage Crash")
-  plot(temp$month[temp$CrashTypeName == "Property Damage Crash" & temp$Year == i], 
-       temp$sum[temp$CrashTypeName == "Property Damage Crash" & temp$Year == i],
-       xlab = "Month", ylab = "Number of crashes", main = name, col = i+5,
-       pch = '*', type = 'o')
-}
+#par(mar = rep(2, 4))
+#par(mfrow=c(2,3))
+#for (i in c(2015:2010)){
+#  name  = paste(i, " Property Damage Crash")
+#  plot(temp$month[temp$CrashTypeName == "Property Damage Crash" & temp$Year == i], 
+#       temp$sum[temp$CrashTypeName == "Property Damage Crash" & temp$Year == i],
+#       xlab = "Month", ylab = "Number of crashes", main = name, col = i+5,
+#       pch = '*', type = 'o')
+#}
 
 
-###### plot by location ######
+
+
+
+
+
+######### plot by location #########
 # total crash of the year 
 par(mar = rep(2, 4))
 par(mfrow=c(2,3))
